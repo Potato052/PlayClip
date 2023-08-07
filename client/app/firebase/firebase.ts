c08ee486e92ddc9b5d417d4b00daf2e496a8e25a
Potato052 <nigeltan2002@yahoo.com.hk>
2023-08-07 16:30:28 +0800

    firebase google auth

---
+++ client/app/firebase/firebase.ts
@@ -0,0 +1,38 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, User } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAhBTXxPWq0dwzudMoPuoAG5VNVXanbm6Y",
+    authDomain: "playclip.firebaseapp.com",
+    projectId: "playclip",
+    appId: "1:252854328896:web:6bc31b4b69a9f76e8489a5"
+  };
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+/**
+ * Signs the user in with a Google popup.
+ * @returns A promise that resolves with the user's credentials.
+ */
+export function signInWithGoogle() {
+  return signInWithPopup(auth, new GoogleAuthProvider());
+}
+
+/**
+ * Signs the user out.
+ * @returns A promise that resolves when the user is signed out.
+ */
+export function signOut() {
+  return auth.signOut();
+}
+
+/**
+ * Trigger a callback when user auth state changes.
+ * @returns A function to unsubscribe callback.
+ */
+export function onAuthStateChangedHelper(callback: (user: User | null) => void) {
+  return onAuthStateChanged(auth, callback);
+}
--- client/components/navbar.tsx
+++ client/components/navbar.tsx
@@ -1,19 +1,38 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import SearchBar from "./searchbar";
+import { User } from "firebase/auth";
+import { signInWithGoogle, signOut, onAuthStateChangedHelper } from "../app/firebase/firebase";
 
 export default function Navbar() {
+  const [user, setUser] = useState<User | null>(null);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedHelper((user) => {
+      setUser(user);
+    });
+
+    // Cleanup subscription on unmount
+    return () => unsubscribe();
+  }, []);
   return (
     <nav className="flex justify-between items-center p-4">
       <Link href="/">
         <Image width={90} height={20} src="/logo.svg" alt="YouTube Logo" />
       </Link>
       <SearchBar />
-      <button className="border border=gray px-4 py-2 rounded-full font-bold text-base cursor-pointer">
-        Sign In
-      </button>
+      {user ? (
+        // If user is signed in, show a welcome message (or something else)
+        <button  className="border border=gray px-4 py-2 rounded-full font-bold text-base cursor-pointer" onClick={signOut}>
+          Sign Out
+        </button>
+      ) : (
+        // If user is not signed in, show sign-in button
+        <button  className="border border=gray px-4 py-2 rounded-full font-bold text-base cursor-pointer" onClick={signInWithGoogle}>
+          Sign in
+        </button>
+      )}
     </nav>
   );
 }
